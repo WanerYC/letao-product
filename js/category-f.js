e163aded1f80e301e5d0e3aa8c712da09be70b7d
ChunYnag <yangchun920726@126.com>
2018-06-20 14:59:42 +0800

    完成分页

--- js/category-f.js
+++ js/category-f.js
@@ -3,20 +3,54 @@ isLogin();
 $(function() {
 
     var page = 1;
-    var pageSize = 10;
-    // alert(1);
-    $.ajax({
-        type: 'get',
-        url: `${APP.baseUrl}/category/queryTopCategoryPaging`,
-        data: {
-            page : page,
-            pageSize : pageSize
-        },
-        success: function(response) {
-            console.log(response);
-            var html = template('categTpl', response);
-            $('#categBox').html(html);
+    var pageSize = 5;
+    var totalP = 0;
+
+    // 渲染页面
+    getData();
+
+    // 分页效果
+    $('#prv').on('click', function() {
+        //  alert(1); 
+        page--;
+        if (page < 1) {
+            page = 1;
+            alert('已经到第一页了,亲');
+            return;
         }
+        getData();  
+    });
+    $('#next').on('click', function() {
+        // alert(2);
+        page++;
+        if (page > totalP) {
+            page = totalP;
+            alert('已经到最后一页了,亲');
+            return;
+        }
+        getData();
     });
 
+    
+    function getData () {
+        $.ajax({
+            type: 'get',
+            url: `${APP.baseUrl}/category/queryTopCategoryPaging`,
+            data: {
+                page : page,
+                pageSize : pageSize
+            },
+            success: function(response) {
+                // console.log(response);
+                /* if (response.success) { */
+                    console.log(response);
+                    var html = template('categTpl', response);
+                    $('#categBox').html(html);
+               /*  }else {
+                    alert(response.message);
+                } */
+                totalP = Math.ceil(response.total / pageSize);
+            }
+        });
+    }
 });
